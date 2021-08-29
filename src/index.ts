c4e0901f1aa83c7ca6b44ab6b5d257b24dfb3750
Thinh Pham <thinh.pham@corize.co.jp>
2021-08-29 23:38:46 +0900

    Primitive types

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,15 @@
-import * as Mailer from './mailer';
+// Primitive types
 
-const age: number = 1;
+const myName: string = 'CodersX';
+const age: number = 3;
+const bigNum: bigint = BigInt(10);
+const isFree: boolean = true;
+const someVar: undefined = undefined;
+const anotherVar: null = null;
 
-let newVar: string;
-
-function sum(a: number, b: number): number {
-  return a + b;
-}
-
-const multiply = (a: number, b: number): number => a * b;
-
-// destructuring ES6
-const sendEmail = async ({
-  email,
-  subject,
-  content,
-}: {
-  email: string;
-  subject: string;
-  content: string;
-}): Promise<boolean> => {
-  const res = await Mailer.send(email, subject, content);
-  return res;
-};
-
-function waitAndDo(callback: (param: string) => void) {
-  setTimeout(() => {
-    callback('Oops!');
-  }, 1000);
-}
-class Dog {
-  sayHi(name: string) {
-    console.log('Gau gau', name);
-  }
-}
+console.log(typeof myName);
+console.log(typeof age);
+console.log(typeof bigNum);
+console.log(typeof isFree);
+console.log(typeof someVar);
+console.log(typeof anotherVar);
