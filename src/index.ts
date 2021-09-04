9a6ed0bdc9d49be43bb649ea6251fd17a79fb92d
Thinh Pham <thinh.pham@corize.co.jp>
2021-09-05 00:42:09 +0900

    Optional, strictNullCheck

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 // interface
+// optional properties
+import { send } from './mailer';
+
 interface Contact {
   name: string;
   phone: string;
-  email: string;
+  email?: string;
 }
 
 const contacts: Contact[] = [];
@@ -13,4 +16,13 @@ const newContact: Contact = {
   email: 'abc@gmail.com',
 };
 
+const otherContact: Contact = {
+  name: 'Nguyen Van B',
+  phone: '0123124',
+};
+
 contacts.push(newContact);
+
+if (newContact.email) {
+  send(newContact.email, '1', '2');
+}
